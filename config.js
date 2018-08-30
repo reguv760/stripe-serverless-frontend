67fa8a7f21c4e2ebc36c157879b419466104d5f8
reguv760 <reguv760@gmail.com>
2018-08-29 19:29:33 -0700

    serverless RG : 8/29

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   stripe: {
-    apiKey: 'pk_test_SG9qh1GK5AiTycr7x4t3L9p6',
-    apiUrl: 'https://kpygs0yhak.execute-api.ap-southeast-1.amazonaws.com/dev/charges',
+    apiKey: 'pk_test_J0ETUVIepcuVocZb2ZqNJ263',
+    apiUrl: 'https://97436kbte3.execute-api.us-west-2.amazonaws.com/prod/charge',
     currency: 'USD',
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default () => (
   <Layout>
     <h1>Serverless Stripe Checkout</h1>
     <p>Use test@email.com, 4242 4242 4242 4242, and any CVC and future expiration date.</p>
-    <PayButton amount={200} />
+    <PayButton amount={20000} />
     <p><a href="https://yos.io/2017/06/22/serverless-stripe/">Read the blog post!</a></p>
   </Layout>
 );
